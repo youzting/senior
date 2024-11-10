8ff3939272408386da26cbfde608405fbad60b38
youzting <158013348+youzting@users.noreply.github.com>
2024-11-11 06:17:44 +0900

    Create WebSocketConfig.java

---
+++ WebSocketConfig.java
@@ -0,0 +1,24 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메시지 브로커를 설정하여 /topic/messages와 같은 토픽을 사용할 수 있게 함
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 클라이언트가 접속할 수 있는 WebSocket 엔드포인트 등록
+        registry.addEndpoint("/chat").withSockJS();
+    }
+}
+
