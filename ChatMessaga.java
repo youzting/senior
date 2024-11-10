991a2d3e4ba08532196ea8e35024ccadde462f42
youzting <158013348+youzting@users.noreply.github.com>
2024-11-11 06:16:57 +0900

    Create ChatMessaga.java

---
+++ ChatMessaga.java
@@ -0,0 +1,31 @@
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private String recipient;
+
+    // Getters and Setters
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+}
