8b30cc6948ed4b787990e0535c975cbaaa0455c9
youzting <158013348+youzting@users.noreply.github.com>
2024-12-05 21:56:10 +0900

    Update Server.js

--- Server.js
+++ Server.js
@@ -106,7 +106,7 @@ app.post('/signup/insert', async (req, res) => {
     const { username, password, email, phone, birthdate, age, gender, interests, health_conditions } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     db.query('INSERT INTO users (username, password, email, phone, birthdate, age, gender, interests, health_conditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [username, password, email, phone, birthdate, age, gender, interests, health_conditions],
+        [username, hashedPassword, email, phone, birthdate, age, gender, interests, health_conditions],
         (err) => {
             if (err) {
                 console.error(err);
