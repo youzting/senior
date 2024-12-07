a90cca840ae39b659cabc6ae72d1ca3d9ad4f756
youzting <158013348+youzting@users.noreply.github.com>
2024-12-07 18:18:09 +0900

    Update Server.js

--- Server.js
+++ Server.js
@@ -169,7 +169,7 @@ app.get('/mypage', isAuthenticated, (req, res) => {
 
             // 데이터베이스에서 가져온 신청 내역을 HTML로 전달
            res.render('mypage', { 
-                user: userResults[0], 
+                userResults: userResults[0], 
                 applicationResults: applicationResults 
             });
         });
