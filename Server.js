e199838f50bca7604be0b364df0fa40ab567c091
comugong <165180439+comugong@users.noreply.github.com>
2024-12-08 21:56:38 +0900

    Update Server.js

--- Server.js
+++ Server.js
@@ -253,7 +253,7 @@ app.post('/appform', isAuthenticated, (req, res) => {
 });
 
 // 기타 페이지 라우팅
-const pages = ['hobbyRec', 'program', 'progApply', 'progInfo1', 'progInfo2', 'parent', 'child'];
+const pages = ['hobbyRec', 'program', 'progApply', 'progInfo1', 'progInfo2', 'parent', 'child', 'posts'];
 pages.forEach(page => {
     app.get(`/${page}`, (req, res) => {
         res.sendFile(path.join(__dirname, 'public', `${page}.html`));
